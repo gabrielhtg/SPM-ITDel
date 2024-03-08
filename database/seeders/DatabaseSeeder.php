21a5e7b584473c31b88273e19ec4d50f93be14af
unknown <ifs21014@students.del.ac.id>
2024-03-08 22:27:04 +0700

    dokumen seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use DokumenSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -25,6 +26,7 @@ class DatabaseSeeder extends Seeder
             allowedUser::class,
             DocumentTypesTableSeeder::class,
             DocType::class,
+            Dokumen::class,
         ]);
     }
 }
