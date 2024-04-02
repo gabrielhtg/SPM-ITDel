105027e8456de2a5a17849ca1b33bf119946fa2a
Rio Eka Pasaribu <112598698+RioEkaPasaribu@users.noreply.github.com>
2024-04-02 18:37:13 +0700

    Update Minor

--- resources/views/document-management-add.blade.php
+++ resources/views/document-management-add.blade.php
@@ -61,7 +61,7 @@
                     <i class="fas fa-plus"></i> <span style="margin-left: 5px">Tambah Laporan</span>
                 </a>
 
-                <a href="{{ route('documentAdd') }}" class="btn btn-success mb-3">
+                <a href="#modal-success2" class="btn btn-success mb-3" data-toggle="modal">
                     <i class="fas fa-plus"></i> <span style="margin-left: 5px">Tambah Tipe Laporan</span>
                 </a>
 
@@ -69,7 +69,7 @@
                 @if(app(AllServices::class)->isAdmin())
                     @include('components.list-document-pending-modal')
                 @endif
-            
+
 
                 <table id="example1" class="table table-bordered table-striped">
                     <thead>
@@ -178,6 +178,37 @@
                             </div>
                         </div>
                     </div>
+                </div>
+
+                <div class="modal fade" id="modal-success2">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title">Add Document Type</h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form id="form-register" method="POST" action="{{ route('uploadTypeDocument') }}">
+                                    @csrf
+
+                                    <div class="form-group">
+                                        <label for="jenis_dokumen">Document Type</label>
+                                        <input type="text" name="jenis_dokumen" placeholder="Document Type" class="form-control" required>
+                                        <div class="invalid-feedback">Please provide a valid document type.</div>
+                                    </div>
+
+                                    <div class="modal-footer justify-content-between">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="submit" class="btn btn-primary">Add</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <!-- /.modal-content -->
+                    </div>
+                    <!-- /.modal-dialog -->
                 </div>
 
 
