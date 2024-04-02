b6459f08e742a654f0d3dbda86a4b0442d37aac5
geraldnael <geraldnael@gmail.com>
2024-04-02 14:21:18 +0700

    bindo

--- resources/views/components/change-password-modal.blade.php
+++ resources/views/components/change-password-modal.blade.php
@@ -6,7 +6,7 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Change Password</h4>
+                <h4 class="modal-title">Ganti Kata Sandi</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -15,7 +15,7 @@
                 <form id="form-change-password" method="POST" action="{{ route('change-password') }}">
                     @csrf
                     <div class="input-group">
-                        <input type="password" name="current_password" class="form-control" placeholder="Current Password" required autofocus>
+                        <input type="password" name="current_password" class="form-control" placeholder="Kata Sandi Saat Ini" required autofocus>
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <i class="fas fa-lock"></i>
@@ -25,7 +25,7 @@
                     <span class="text-danger">{{ $errors->first('current_password') }}</span>
 
                     <div class="input-group mt-3">
-                        <input type="password" name="password" class="form-control" placeholder="Password" required autofocus>
+                        <input type="password" name="password" class="form-control" placeholder="Kata Sandi Baru" required autofocus>
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <i class="fas fa-lock"></i>
@@ -35,7 +35,7 @@
                     <span class="text-danger">{{ $errors->first('password') }}</span>
 
                     <div class="input-group mt-3">
-                        <input type="password" name="password_confirmation" class="form-control" placeholder="Re-Enter Password" required>
+                        <input type="password" name="password_confirmation" class="form-control" placeholder="Masukkan Kembali Kata Sandi Baru" required>
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <i class="fas fa-lock"></i>
@@ -46,8 +46,8 @@
                 </form>
             </div>
             <div class="modal-footer justify-content-between">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="submit" form="form-change-password" class="btn btn-primary"><i class="far fa-save mr-1"></i> Save</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                <button type="submit" form="form-change-password" class="btn btn-primary"><i class="far fa-save mr-1"></i>Simpan</button>
             </div>
         </div>
         <!-- /.modal-content -->
