cafb924c6091865e97abcff03c76594cade79d1b
Rafael Manurung <rafaelmanurung80@gmail.com>
2024-04-15 20:38:56 +0700

    fixing node tree modal

--- app/Models/TreeData.php
+++ app/Models/TreeData.php
@@ -7,11 +7,17 @@ class TreeData
     public $imageURL;
     public $name;
     public $role;
+    public $responsible;
+    public $accountable;
+    public $informable;
 
-    public function __construct($imageURL, $name, $role)
+    public function __construct($imageURL, $name, $role, $responsible, $accountable, $informable)
     {
         $this->imageURL = $imageURL;
         $this->name = $name;
         $this->role = $role;
+        $this->responsible = $responsible;
+        $this->accountable = $accountable;
+        $this->informable = $informable;
     }
 }
