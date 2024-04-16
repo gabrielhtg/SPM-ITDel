f6cd5dddd59a1f6c2932277525a692d2d6094d9c
ifs21014-itdel <112598389+ifs21014-itdel@users.noreply.github.com>
2024-04-16 08:42:18 +0700

    fixing bug upload laporan

--- resources/views/components/upload-laporan.blade.php
+++ resources/views/components/upload-laporan.blade.php
@@ -2,8 +2,7 @@
     <i class="fas fa-plus"></i> <span style="margin-left: 5px">Tambah Laporan</span>
 </a>
 
-
-
+<!-- Modal -->
 <div class="modal fade" id="modal-add-laporan">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
         <div class="modal-content">
@@ -14,16 +13,15 @@
                 </button>
             </div>
             <div class="modal-body">
-                <form id="form-add-laporan" action="{{ route('laporan.store') }}" method="POST">
+                <!-- Formulir -->
+                <form id="form-add-laporan" action="{{ route('laporan.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
 
                     <div class="form-group">
                         <label for="nama-laporan">Nama Laporan</label>
-                        <input type="text" class="form-control" placeholder="Ketik disini" id="nama-laporan" name="nama_laporan"
-                               required>
+                        <input type="text" class="form-control" placeholder="Ketik disini" id="nama-laporan" name="nama_laporan" required>
                     </div>
 
-                   
                     <div class="form-group mt-3">
                         <label for="tipe-laporan">Tipe Laporan</label>
                         <select id="tipe-laporan" name="id_tipelaporan" 
@@ -34,24 +32,24 @@
                             @endforeach
                         </select>
                     </div>
-                     <div class="form-group">
+
+                    <div class="form-group">
                         <label for="revisi">Revisi:</label><br>
                         <div class="form-check form-check-inline">
                             <input class="form-check-input" type="checkbox" id="revisiCheckbox" name="revisi" value="1">
                             <label class="form-check-label" for="revisiCheckbox">Ya</label>
                         </div>
                     </div>
-			<div class="form-group mt-3" style="display: none;">
+
+                    <div class="form-group mt-3" style="display: none;">
                         <label for="menggantikan">Merevisi Laporan:</label>
                         <select id="menggantikan" name="cek_revisi" 
                                 class="menggantikan-to-custom form-control" style="width: 100%">
                             <option></option>
                             @php
                             $allServices = new \App\Services\AllServices();
-                          
                             @endphp
                             @foreach ($laporan as $item)
-                            @php
                                 @if(auth()->user()->id == $item->created_by && $item->status=="Ditolak" && $allServices->isLaporanIdInCekLaporan($item->id))
                                     <option value="{{$item->id}}">{{$item->nama_laporan}}</option>
                                 @endif
@@ -59,47 +57,37 @@
                         </select>
                     </div>
 
-                
                     <div class="form-group">
                         <label for="document-file">Dokumen</label>
                         <div class="input-group">
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="document-file" name="file" required>
+                                <input type="file" name="file" class="custom-file-input" id="document-file">
                                 <label class="custom-file-label" for="document-file">Pilih file</label>
                             </div>
                         </div>
                     </div>
-
-                </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary" form="document-form">Submit</button>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </div>
+            </form>
         </div>
     </div>
 </div>
 
-
-
-
-
+<!-- Script JavaScript -->
 <script>
-    // Dapatkan elemen checkbox
-    var checkbox = document.getElementById('revisiCheckbox');
+    document.addEventListener("DOMContentLoaded", function() {
+        var checkbox = document.getElementById('revisiCheckbox');
+        var merevisiLaporan = document.getElementById('menggantikan').parentNode;
 
-    // Dapatkan elemen yang akan dimunculkan atau disembunyikan
-    var merevisiLaporan = document.getElementById('menggantikan').parentNode;
-
-    // Tambahkan event listener untuk memantau perubahan pada checkbox
-    checkbox.addEventListener('change', function() {
-        // Jika checkbox dicentang, maka munculkan elemen
-        if (checkbox.checked) {
-            merevisiLaporan.style.display = 'block';
-        } else {
-            // Jika checkbox tidak dicentang, sembunyikan elemen
-            merevisiLaporan.style.display = 'none';
-        }
+        checkbox.addEventListener('change', function() {
+            if (checkbox.checked) {
+                merevisiLaporan.style.display = 'block';
+            } else {
+                merevisiLaporan.style.display = 'none';
+            }
+        });
     });
 </script>
-
