fe4cd0b42b9d8f8b05805eb62a7c9c7d44d2f435
Gabriel Cesar Hutagalung <gabrielhutagalung970@gmail.com>
2024-04-29 21:21:56 +0700

    adding favicon

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,8 +5,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SPM IT Del</title>
-
+    <title>Profile  </title>
+    <link rel="shortcut icon" type="image/jpg" href="{{ asset("src/img/logo.png") }}"/>
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
