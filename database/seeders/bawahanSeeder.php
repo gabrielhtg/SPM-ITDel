7d7925085c2121a7fa77a441d661eb49c258540b
geraldnael <geraldnael@gmail.com>
2024-04-30 09:26:07 +0700

    update

--- database/seeders/bawahanSeeder.php
+++ database/seeders/bawahanSeeder.php
@@ -17,6 +17,7 @@ class bawahanSeeder extends Seeder
             ["role"=>2,"bawahan" => 3],
             ["role"=>2,"bawahan" => 4],
             ["role"=>2,"bawahan" => 6],
+            ["role"=>3,"bawahan" => 4],
             ["role"=>2,"bawahan" => 7],
             ["role"=>2,"bawahan" => 8],
             ["role"=>6,"bawahan" => 9],
