fe91c4e39bebaa83f6a2fbe39efc4b1c06650685
ifs21055-itdel <ifs21055@students.del.ac.id>
2024-05-06 11:51:26 +0700

    memperbaiki sidebar dashboard laporan

--- app/Http/Controllers/DashboardLaporanController.php
+++ app/Http/Controllers/DashboardLaporanController.php
@@ -8,13 +8,13 @@ class DashboardLaporanController extends Controller
 {
     public function index()
     {
-        $active_sidebar = [0, 1]; // atau sesuai dengan kebutuhan Anda
+        $active_sidebar = [0, 0]; // atau sesuai dengan kebutuhan Anda
 
         return view('components.dashboard-laporan', compact('active_sidebar'));
     }
     public function getDashboardLaporanContinue()
     {
-        $active_sidebar = [0, 1]; // atau sesuai dengan kebutuhan Anda
+        $active_sidebar = [0, 0]; // atau sesuai dengan kebutuhan Anda
 
         return view('components.dashboard-laporan-continue', compact('active_sidebar'));
     }
