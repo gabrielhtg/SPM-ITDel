ae4f586e8d15d3f997b4f1bcdb7a3d474a211b15
Rio Eka Pasaribu <112598698+RioEkaPasaribu@users.noreply.github.com>
2024-05-21 14:02:58 +0700

    Update Seeder user Pak Indra

--- database/seeders/LaporanTableSeeder.php
+++ database/seeders/LaporanTableSeeder.php
@@ -16,13 +16,14 @@ class LaporanTableSeeder extends Seeder
     public function run()
     {
         DB::table('laporan')->insert([
+            //Dekan FITE
             [
                 'id_tipelaporan' => 1, // Adjust according to your 'jenis_laporan' table
                 'direview_oleh' => null, // Adjust according to your 'users' table
                 'created_by' => 8, // Adjust according to your 'users' table
                 'nama_laporan' => 'Laporan Bulanan',
-                'directory' => 'reports/monthly',
-                'revisi' => false,
+                'directory' => 'src/documents/Laporan_Bulanan.pdf',
+                'revisi' => 0,
                 'status' => 'Menunggu',
                 'cek_revisi' => null,
                 'approve_at' => null,
@@ -36,15 +37,15 @@ class LaporanTableSeeder extends Seeder
                 'id_tipelaporan' => 2, // Adjust according to your 'jenis_laporan' table
                 'direview_oleh' => 3, // Adjust according to your 'users' table
                 'created_by' => 8, // Adjust according to your 'users' table
-                'nama_laporan' => 'Laporan Mingguan',
-                'directory' => 'reports/weekly',
-                'revisi' => false,
+                'nama_laporan' => 'Laporan Bulanan',
+                'directory' => 'src/documents/Laporan_Bulanan.pdf',
+                'revisi' => 0,
                 'status' => 'Disetujui',
-                'cek_revisi' => 'Perbaikan diperlukan',
+                'cek_revisi' => null,
                 'approve_at' => now(),
                 'reject_at' => null,
-                'komentar' => 'Weekly report has been approved',
-                'file_catatan' => 'notes/revision1.pdf',
+                'komentar' => 'Monthly report has been approved',
+                'file_catatan' => 'Approved',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
@@ -52,19 +53,167 @@ class LaporanTableSeeder extends Seeder
                 'id_tipelaporan' => 3, // Adjust according to your 'jenis_laporan' table
                 'direview_oleh' => 3, // Adjust according to your 'users' table
                 'created_by' => 8, // Adjust according to your 'users' table
-                'nama_laporan' => 'Laporan Harian',
-                'directory' => 'reports/daily',
-                'revisi' => true,
+                'nama_laporan' => 'Laporan Bulanan',
+                'directory' => 'src/documents/Laporan_Bulanan.pdf',
+                'revisi' => 1,
                 'status' => 'Direview',
+                'cek_revisi' => 'Perbaikan diperlukan',
+                'approve_at' => null,
+                'reject_at' => null,
+                'komentar' => 'Monthly report declined',
+                'file_catatan' => 'revisi',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            //Dekan FTI
+            [
+                'id_tipelaporan' => 1, // Adjust according to your 'jenis_laporan' table
+                'direview_oleh' => null, // Adjust according to your 'users' table
+                'created_by' => 10, // Adjust according to your 'users' table
+                'nama_laporan' => 'Laporan Bulanan',
+                'directory' => 'src/documents/Laporan_Bulanan.pdf',
+                'revisi' => 0,
+                'status' => 'Menunggu',
                 'cek_revisi' => null,
                 'approve_at' => null,
                 'reject_at' => null,
-                'komentar' => 'Daily report under review',
+                'komentar' => 'Initial report submission',
                 'file_catatan' => null,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
-            // Add more entries as needed
+            [
+                'id_tipelaporan' => 2, // Adjust according to your 'jenis_laporan' table
+                'direview_oleh' => 3, // Adjust according to your 'users' table
+                'created_by' => 10, // Adjust according to your 'users' table
+                'nama_laporan' => 'Laporan Bulanan',
+                'directory' => 'src/documents/Laporan_Bulanan.pdf',
+                'revisi' => 0,
+                'status' => 'Disetujui',
+                'cek_revisi' => null,
+                'approve_at' => now(),
+                'reject_at' => null,
+                'komentar' => 'Monthly report has been approved',
+                'file_catatan' => 'notes/revision1.pdf',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_tipelaporan' => 3, // Adjust according to your 'jenis_laporan' table
+                'direview_oleh' => 3, // Adjust according to your 'users' table
+                'created_by' => 10, // Adjust according to your 'users' table
+                'nama_laporan' => 'Laporan Bulanan',
+                'directory' => 'src/documents/Laporan_Bulanan.pdf',
+                'revisi' => 1,
+                'status' => 'Direview',
+                'cek_revisi' => 'Perbaikan diperlukan',
+                'approve_at' => null,
+                'reject_at' => null,
+                'komentar' => 'Monthly report declined',
+                'file_catatan' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            //Dekan Fakultas Vokasi
+            [
+                'id_tipelaporan' => 1, // Adjust according to your 'jenis_laporan' table
+                'direview_oleh' => null, // Adjust according to your 'users' table
+                'created_by' => 9, // Adjust according to your 'users' table
+                'nama_laporan' => 'Laporan Bulanan',
+                'directory' => 'src/documents/Laporan_Bulanan.pdf',
+                'revisi' => 0,
+                'status' => 'Menunggu',
+                'cek_revisi' => null,
+                'approve_at' => null,
+                'reject_at' => null,
+                'komentar' => 'Initial report submission',
+                'file_catatan' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_tipelaporan' => 2, // Adjust according to your 'jenis_laporan' table
+                'direview_oleh' => 3, // Adjust according to your 'users' table
+                'created_by' => 9, // Adjust according to your 'users' table
+                'nama_laporan' => 'Laporan Bulanan',
+                'directory' => 'src/documents/Laporan_Bulanan.pdf',
+                'revisi' => 0,
+                'status' => 'Disetujui',
+                'cek_revisi' => null,
+                'approve_at' => now(),
+                'reject_at' => null,
+                'komentar' => 'Monthly report has been approved',
+                'file_catatan' => 'notes/revision1.pdf',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_tipelaporan' => 3, // Adjust according to your 'jenis_laporan' table
+                'direview_oleh' => 3, // Adjust according to your 'users' table
+                'created_by' => 9, // Adjust according to your 'users' table
+                'nama_laporan' => 'Laporan Bulanan',
+                'directory' => 'src/documents/Laporan_Bulanan.pdf',
+                'revisi' => 1,
+                'status' => 'Direview',
+                'cek_revisi' => 'Perbaikan diperlukan',
+                'approve_at' => null,
+                'reject_at' => null,
+                'komentar' => 'Monthly report declined',
+                'file_catatan' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            //Dekan Fakultas Bioteknologi
+            [
+                'id_tipelaporan' => 1, // Adjust according to your 'jenis_laporan' table
+                'direview_oleh' => null, // Adjust according to your 'users' table
+                'created_by' => 11, // Adjust according to your 'users' table
+                'nama_laporan' => 'Laporan Bulanan',
+                'directory' => 'src/documents/Laporan_Bulanan.pdf',
+                'revisi' => 0,
+                'status' => 'Menunggu',
+                'cek_revisi' => null,
+                'approve_at' => null,
+                'reject_at' => null,
+                'komentar' => 'Initial report submission',
+                'file_catatan' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_tipelaporan' => 2, // Adjust according to your 'jenis_laporan' table
+                'direview_oleh' => 3, // Adjust according to your 'users' table
+                'created_by' => 11, // Adjust according to your 'users' table
+                'nama_laporan' => 'Laporan Bulanan',
+                'directory' => 'src/documents/Laporan_Bulanan.pdf',
+                'revisi' => 0,
+                'status' => 'Disetujui',
+                'cek_revisi' => null,
+                'approve_at' => now(),
+                'reject_at' => null,
+                'komentar' => 'Monthly report has been approved',
+                'file_catatan' => 'notes/revision1.pdf',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_tipelaporan' => 3, // Adjust according to your 'jenis_laporan' table
+                'direview_oleh' => 3, // Adjust according to your 'users' table
+                'created_by' => 11, // Adjust according to your 'users' table
+                'nama_laporan' => 'Laporan Bulanan',
+                'directory' => 'src/documents/Laporan_Bulanan.pdf',
+                'revisi' => 1,
+                'status' => 'Direview',
+                'cek_revisi' => 'Perbaikan diperlukan',
+                'approve_at' => null,
+                'reject_at' => null,
+                'komentar' => 'Monthly report declined',
+                'file_catatan' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
     }
 }
