cc178f958e903002039a90a9e89689384328dc19
ifs21055-itdel <ifs21055@students.del.ac.id>
2024-05-23 16:17:02 +0700

    menambahkan shortcut icon

--- resources/views/document-view.blade.php
+++ resources/views/document-view.blade.php
@@ -7,6 +7,7 @@
     <title>Document</title>
 
     <!-- Google Font: Source Sans Pro -->
+    <link rel="shortcut icon" type="image/jpg" href="{{ asset("src/img/logo.png") }}"/>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset("plugins/fontawesome-free/css/all.min.css") }}">
